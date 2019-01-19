1ab9355b657c20ef42338f48fab597d198bf7f04
Jason Tashman <jtashman703@gmail.com>
2019-01-19 09:54:16 -0500

    added authentication code

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 var express = require("express");
-// var exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var passport = require('passport');
 
 var db = require("./models");
 
@@ -9,21 +13,38 @@ var PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: "aldkfbuunei",
+    resave: false,
+    saveUninitialized: false,
+    // cookie: { secure: true }
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+var Users = require("./routes/Users.js");
+
+app.user("/users", Users);
 
 // Handlebars
-// app.engine(
-//   "handlebars",
-//   exphbs({
-//     defaultLayout: "main"
-//   })
-// );
-// app.set("view engine", "handlebars");
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/apiRoutes.js")(app);
-require("./routes/htmlRoutes.js")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
 
