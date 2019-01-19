00b57e4a7aa811ee06d048fb686623ba400a5ba4
natedogg484 <nathangschmidt@gmail.com>
2019-01-19 12:53:56 -0500

    jquery posts

--- models/job.js
+++ models/job.js
@@ -1,20 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
   var Job = sequelize.define("Job", {
-    job_title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1, 35]
-      }
-    },
     company_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1, 25]
-      }
-    },
-    job_link: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
@@ -28,11 +14,44 @@ module.exports = function(sequelize, DataTypes) {
         len: [1, 180]
       }
     },
-   salary: {
+    job_link: {
       type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 25]
+      }
+    },
+    salary: {
+      type: DataTypes.INTEGER,
       allowNull: true,
       validate: {
         len: [1, 45]
+      }
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [1, 25]
+      }
+    },
+    applied: {
+      type: DataTypes.BOOLEAN
+    },
+    pre_interview: {
+      type: DataTypes.BOOLEAN
+    },
+    interview: {
+      type: DataTypes.BOOLEAN
+    },
+    offer: {
+      type: DataTypes.BOOLEAN
+    },
+    comments: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
       }
     }
   });
