3e3b9d158ce84e091506406438f8755e3de87424
natedogg484 <nathangschmidt@gmail.com>
2019-01-19 10:59:42 -0500

    routes beginning

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,19 +5,24 @@
 // Dependencies
 // =============================================================
 
-// Requiring our Todo model
+// Requiring models
 var db = require("../models");
 
 // Routes
 // =============================================================
 module.exports = function(app) {
+  // GET route for getting all of the job posts
+  app.get("/api/Jobs/", function(req, res) {
+    db.Job.findAll().then(function(dbJob) {
+      res.json(dbJob);
+    });
+  });
 
-  // GET route for getting all of the posts
-  app.get("/api/posts/", function(req, res) {
-    db.Post.findAll({})
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
+  // GET route for getting all of the BaconeerInfo
+  app.get("/api/BaconeerInfo/", function(req, res) {
+    db.BaconeerInfo.findAll().then(function(dbBaconeerInfo) {
+      res.json(dbBaconeerInfo);
+    });
   });
 
   // Get route for returning posts of a specific category
@@ -26,10 +31,9 @@ module.exports = function(app) {
       where: {
         category: req.params.category
       }
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
+    }).then(function(dbPost) {
+      res.json(dbPost);
+    });
   });
 
   // Get route for retrieving a single post
@@ -38,47 +42,46 @@ module.exports = function(app) {
       where: {
         id: req.params.id
       }
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
+    }).then(function(dbPost) {
+      res.json(dbPost);
+    });
   });
 
   // POST route for saving a new post
   app.post("/api/posts", function(req, res) {
     console.log(req.body);
-    db.Post.create({
-      title: req.body.title,
-      body: req.body.body,
-      category: req.body.category
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
+    db.Job.create({
+      company_name: req.body.company_name,
+      job_title: req.body.job_title,
+      job_link: req.body.job_link,
+      desciription: req.body.description,
+      salary: req.body.salary
+    }).then(function(dbJob) {
+      res.json(dbJob);
+    });
   });
 
-  // DELETE route for deleting posts
-  app.delete("/api/posts/:id", function(req, res) {
-    db.Post.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
+  // POST route for saving a new post
+  app.post("/api/posts", function(req, res) {
+    console.log(req.body);
+    db.BaconeerInfo.create({
+      email: req.body.email,
+      phoneNumber: req.body.phoneNumber,
+      firstnaeme: req.body.firstName,
+      lastName: req.body.lastName
+    }).then(function(dbBaconeerInfo) {
+      res.json(dbBaconeerInfo);
+    });
   });
 
   // PUT route for updating posts
   app.put("/api/posts", function(req, res) {
-    db.Post.update(req.body,
-      {
-        where: {
-          id: req.body.id
-        }
-      })
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
+    db.Post.update(req.body, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbPost) {
+      res.json(dbPost);
+    });
   });
 };
